329efd8a41f8a44647746462ed156bb1c15050c1
KrisiKostadinov <krisi.199898@gmail.com>
2020-06-06 19:47:16 +0300

    Add saving to local storage to dos.

--- Todo list/main.js
+++ Todo list/main.js
@@ -1,18 +1,54 @@
 const getListItemPath = 'views/list-item.hbs';
 
+var todos = [];
+
+function saveTodo(todo) {
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+function getTodos() {
+    todos = JSON.parse(localStorage.getItem('todos'));
+    if(todos === null) {
+        todos = [];
+    }
+
+    todos.forEach(todo => {
+        $.get(getListItemPath).then(function(src) {
+            var li = document.createElement('li');
+            var templateHbs = Handlebars.compile(src);
+            var html = templateHbs(todo);
+            li.innerHTML = html;
+            li.setAttribute('id', todo.id);
+            
+            li.querySelector('button.dismiss').addEventListener('click', dismissTask);
+            li.querySelector('button.success').addEventListener('click', checkedTask);
+
+            $('ul').append(li);
+        }); 
+    });
+}
+
+getTodos();
+
+isEmptyList();
+
 (() => {
-    isEmptyList();
     $('.todos-container > button').click(() => addForm());
     $('form > button').click(() => addTask(event));
+    $('.todos-container > h2').text(`To do list ${todos.length}`);
 })();
 
 function dismissTask() {
+    var id = this.parentElement.parentElement.id;
+    todos = todos.filter(x => x.id !== parseInt(id));
+    localStorage.setItem('todos', JSON.stringify(todos));
     $(this).parent().parent().remove();
     isEmptyList();
 }
 
 function isEmptyList() {
-    document.querySelector('ul').children.length === 0 ? $('h4').show(200) : $('h4').hide(200);
+    todos.length === 0 ? $('h4').show(200) : $('h4').hide(200);
 }
 
 function checkedTask() {
@@ -31,15 +67,20 @@ function addTask(event) {
         $.get(getListItemPath).then(function(src) {
             var li = document.createElement('li');
             var templateHbs = Handlebars.compile(src);
-            var context = { content: task };
+            var context = { content: task, id: todos.length + 1 };
             var html = templateHbs(context);
             li.innerHTML = html;
+            
+            saveTodo(context);
             
             li.querySelector('button.dismiss').addEventListener('click', dismissTask);
             li.querySelector('button.success').addEventListener('click', checkedTask);
 
             $('ul').append(li);
             
+            var element = document.querySelector("ul");
+            element.scrollTop = element.scrollHeight;
+
             isEmptyList();
          });
 
